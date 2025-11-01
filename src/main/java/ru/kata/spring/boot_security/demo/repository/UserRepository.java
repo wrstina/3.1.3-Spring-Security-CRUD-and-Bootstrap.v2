3092e170ddab5ffbc4e170a73941ed30f0b5fe55
Christina <olenevac@mail.ru>
2025-11-01 05:37:47 +0500

    fix dto and n+1

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -1,10 +1,20 @@
 package ru.kata.spring.boot_security.demo.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.kata.spring.boot_security.demo.entity.User;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     boolean existsByUsernameIgnoreCase(String username);
+
+    @Override
+    @EntityGraph(attributePaths = "roles")
+    List<User> findAll();
 }
+
